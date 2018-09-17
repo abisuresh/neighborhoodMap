5540eeebf90d986f60d0c52d78787dae9372a5a4
Abi Suresh <aesther2012@gmail.com>
2018-09-17 10:55:35 -0400

    initial create app with some minor edits

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import {GoogleApiWrapper} from 'google-maps-react';
+import {GoogleMap, Marker } from 'react-google-maps';
+import './App.css';
+
+class App extends Component {
+
+    state = {
+
+    }
+
+     startMap(){
+      let location = {}
+      // let map = new google.maps.Map()
+      // let locationMarker = new google.maps.Marker()
+}
+
+  render() {
+    const mapStyle={
+        width: '100%',
+        height: '100%',
+        border: '1px solid black'
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Google Map</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div class="map-container" style = {mapStyle}>
+            <p>
+            </p>
+            <div class="map">
+            </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
